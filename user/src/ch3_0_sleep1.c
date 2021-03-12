b23539a42bbd92a3ab3809b56994f49d6aa27057
nzpznk <skynnznk12@163.com>
2021-03-12 23:22:23 +0800

    fix usershell & %lu

--- user/src/ch3_0_sleep1.c
+++ user/src/ch3_0_sleep1.c
@@ -4,10 +4,10 @@
 
 int main() {
     uint64 start = get_time();
-    printf("current time_msec = %lu\n", start);
+    printf("current time_msec = %d\n", start);
     sleep(100);
     uint64 end = get_time();
-    printf("time_msec = %lu after sleeping 100 ticks, delta = %lums!\n", end, end - start);
+    printf("time_msec = %d after sleeping 100 ticks, delta = %dms!\n", end, end - start);
     puts("TEST sleep1 passed!\n");
     return 0;
 }
--- user/src/ch5_getchar.c
+++ user/src/ch5_getchar.c
@@ -4,14 +4,14 @@
 const uint64 N = 10;
 
 int main() {
-    printf("please enter %lu letters.\n", N);
+    printf("please enter %d letters.\n", N);
     char line[N];
     memset(line, 0, N);
     for (int idx = 0; idx < N; ++idx) {
         char c = getchar();
         line[idx] = c;
     }
-    printf("%lu letters entered\n", N);
+    printf("%d letters entered\n", N);
     for (int idx = 0; idx < N; ++idx) {
         putchar(line[idx]);
     }
