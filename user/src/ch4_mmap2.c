4fcf4ba06467f72d9f57210500cc639c6ac4d492
Yiren Zhang <43681601+DeathWish5@users.noreply.github.com>
2021-03-23 21:47:18 +0800

    Update ch4_mmap2.c

--- user/src/ch4_mmap2.c
+++ user/src/ch4_mmap2.c
@@ -15,7 +15,7 @@ int main()
     assert(len == mmap((void *)start, len, prot));
     uint8 *addr = (uint8 *)start;
     // *addr = (uint8)start; // can't write, R == 0 && W == 1 is illegal in riscv
-    puts("If core dumped, Test 04_3 OK!");
     assert(*addr != 0);
+    puts("Should cause error, Test 04_2 fail!")
     return 0;
 }
