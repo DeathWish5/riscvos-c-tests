4e1ef1bbbf51641ee7ea96950250553a5c0c481b
Deathwish5 <zyr_ms@outlook.com>
2021-04-28 15:10:41 +0800

    bug fix: ch8_06

--- user/src/ch8_06.c
+++ user/src/ch8_06.c
@@ -4,7 +4,7 @@ const uint64 START = 0x10000;
 const uint64 LEN = 0x10000;
 
 void mmap_test(int i) {
-    char* a;
+    char* a = (void*)START;
     *(a + (hash(i) % LEN)) = 'a';
     *(a + (hash(i + 1) % LEN));
 }
